9f9de048d6cf861b0df37918a75dcd6cb3135545
avengermojo <AvengerMoJo@gmail.com>
2016-07-03 08:38:50 +0800

    Add sls file with pyobject format for ceph-cfg
    Signed-off-by: avengermojo <AvengerMoJo@gmail.com>

--- script/ceph-cfg/pyobject.py
+++ script/ceph-cfg/pyobject.py
@@ -1,9 +1,6 @@
 #!pyobjects
 
-Pkg.installed("ceph")
-Pkg.installed("salt-ceph")
-
-with Pkg.installed("python-ceph-cfg"):
+with Pkg.installed("ceph", names=["ceph", "salt-ceph", "python-ceph-cfg"]):
         File.managed("/etc/ceph/ceph.conf", source='file://home/ceph/.ceph_sles_cluster_config/ceph.conf', user='root', group='root', mode='644', makedir=True)
 
 
