ed11a118eb56e27fef8e71c817917464d5650aee
avengermojo <avengermojo@gmail.com>
2017-07-07 22:28:40 +0800

    Fix excluded filter
    Signed-off-by: avengermojo <avengermojo@gmail.com>

--- runner/lttng.py
+++ runner/lttng.py
@@ -120,8 +120,8 @@ def _exclude_filter(excluded):
                             "[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4]"
                             "[0-9]|25[0-5])(\/([0-9]|[1-2][0-9]|3[0-2]))$")
     pat_hostlist = re.compile("^(([a-zA-Z]|[a-zA-Z][a-zA-Z0-9-]*[a-zA-Z0-9])"
-                              ".)*([A-Za-z]|[A-Za-z][A-Za-z0-9-]*[A-Za-z0-9])$"
                               ".)*([A-Za-z]|[A-Za-z][A-Za-z0-9-]*[A-Za-z0-9])$")
+
     compound = []
     ipcidr = []
     iplist = []
